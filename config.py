863ee03f53cea69a78bb324450943a9f24eb7d2f
arvidsson <arvidsson@gmail.com>
2017-11-25 16:14:38 +0100

    Work on supporting config file

---
+++ config.py
@@ -0,0 +1,3 @@
+lib = "io"
+output_file = "io.h"
+namespace = "io"
--- header_only.py
+++ header_only.py
@@ -5,42 +5,45 @@
 Creates a single header from multiple C/C++ files.
 """
 
-from importlib import import_module
+import os
+import importlib
 import argparse
+import sys
+sys.dont_write_bytecode = True
 
-use_config = True
-try:
-    import config
-except ImportError:
-    use_config = False
+use_config = os.path.isfile("config.py")
+if use_config:
+	config_file = importlib.import_module("config")
 
-template_file = ''
-output_file = ''
-tags = {
-    'insert_headers': '/* INTERFACE */',
-    'insert_source': '/* IMPLEMENTATION */',
-    'begin': '/* BEGIN */',
-    'end': '/* END */'
-}
+includes = []
 header_files = []
 source_files = []
 
-def set_defaults():
-    template_file = 'template.h'
-    output_file = 'single_header.h'
-
 def configure():
-    if use_config is False:
-        return
+	config = {
+		'lib': 'lib',
+		'output_file': 'lib.h',
+		'namespace': None,
+		'tag_begin': "/* BEGIN */",
+		'tag_end': "/* END */",
+	}
+	if use_config:
+		if hasattr(config_file, "lib"):
+			config['lib'] =  getattr(config_file, "lib")
+		if hasattr(config_file, "tag_begin") is True:
+			tag_begin = getattr(config_file, "tag_begin")
+		if hasattr(config_file, "tag_end"):
+			tag_end = getattr(config_file, "tag_end")
+	return config
 
 def process_file(path):
     lines = []
     with open(path, "r") as infile:
         found_marker = False
         for line in infile:
-            if line.strip() == tags['begin']:
+            if line.strip() == tag_begin:
                 found_marker = True
-            elif line.strip() == tags['end']:
+            elif line.strip() == tag_end:
                 break
             elif found_marker is True:
                 lines += line
@@ -50,19 +53,72 @@ def process_file(path):
     return lines
 
 def process_files(files):
-    data = []
-    for file in files:
-        data.extend(process_file("src/" + file))
-    data = data[:-1]
-    return "".join(data)
+	data = []
+	for file in files:
+		data.extend(process_file("src/" + file))
+	data = data[:-1]
+	return "".join(data)
+	
+def newline(out):
+	out.write("\n")
+	
+def begin_headerguard(out, headerguard):
+	out.write("#ifndef " + headerguard)
+	newline(out)
+	
+def end_headerguard(out, headerguard):
+	out.write("#endif // " + headerguard)
+	newline(out)
+	
+def begin_extern_c(out):
+	out.write("#ifdef __cplusplus")
+	newline(out)
+	out.write("extern \"C\" {")
+	newline(out)
+	out.write("#endif")
+	newline(out)
 
-def make_header_only_library(interface_source, implementation_source):
-    with open("tools/template.h", "r") as infile, open("io.h", "w") as outfile:
-        data = infile.read().replace("/*INTERFACE*/\n", interface_source)
-        data = data.replace("/*IMPLEMENTATION*/\n", implementation_source)
-        outfile.write(data)
-    
+def end_extern_c(out):
+	out.write("#ifdef __cplusplus")
+	newline(out)
+	out.write("}")
+	newline(out)
+	out.write("#endif")
+	newline(out)
+	
+def begin_namespace(out, namespace):
+	out.write("namespace " + namespace)
+	newline(out)
+	
+def end_namespace(out, namespace):
+	out.write("} // namespace " + namespace)
+	newline(out)
+	
+def begin_implementation(out, define):
+	out.write("#ifdef " + define)
+	newline(out)
+	
+def end_implementation(out, define):
+	out.write("#endif // " + define)
+	newline(out)
+
+def make_header_only_library(config):
+	with open(config['output_file'], "w") as out:
+		begin_headerguard(out, config['lib'].upper() + "_HEADER"), newline(out)
+		begin_extern_c(out), newline(out)
+		if config['namespace'] is not None:
+			begin_namespace(out, config['lib'].lower()), newline(out)
+		# TODO: insert interface
+		begin_implementation(out, config['lib'].upper() + "_IMPLEMENTATION"), newline(out)
+		# TODO: insert implementation
+		end_implementation(out, config['lib'].upper() + "_IMPLEMENTATION"), newline(out)
+		if config['namespace'] is not None:
+			end_namespace(out, config['lib'].lower()), newline(out)
+		end_extern_c(out), newline(out)
+		end_headerguard(out, config['lib'].upper() + "_HEADER")
+	
 if __name__ == '__main__':
-    configure()
-    parser = argparse.ArgumentParser()
-    
+	config = configure()
+	make_header_only_library(config)
+	#parser = argparse.ArgumentParser()
+	
